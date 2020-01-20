92cb7d71194b01f3e8d6bf1d3b5b04e38fc75e2f
Sergej Demchenko <rutelun@ya.ru>
2020-01-20 11:56:50 +0300

    Add webinar hooks to enum

--- src/Enums/ZoomWebHook.php
+++ src/Enums/ZoomWebHook.php
@@ -9,5 +9,8 @@ final class ZoomWebHook extends Enum
     const EVENT_MEETING_CREATED = 'meeting.created';
     const EVENT_MEETING_STARTED = 'meeting.started';
     const EVENT_MEETING_ENDED = 'meeting.ended';
+    const EVENT_WEBINAR_CREATED = 'webinar.created';
+    const EVENT_WEBINAR_STARTED = 'webinar.started';
+    const EVENT_WEBINAR_ENDED = 'webinar.ended';
     const EVENT_RECORDING_COMPLETED = 'recording.completed';
 }
