4bb3f9205b8e2d8ae5f69fbdc87d12d4fb348f5f
Chad Windnagle <cwindnagle@gowithadvanced.com>
2020-04-22 16:11:08 -0400

    clean up exception creation to pull message from response json

--- src/Exceptions/ZoomHttpException.php
+++ src/Exceptions/ZoomHttpException.php
@@ -4,4 +4,9 @@ namespace MacsiDigital\Zoom\Exceptions;
 
 class ZoomHttpException extends \Exception
 {
+    public function __construct($code = 0, $errorResponse = [], \Throwable $previous = null)
+    {
+        $message = $errorResponse['message'];
+        parent::__construct($message, $code, $previous);
+    }
 }
