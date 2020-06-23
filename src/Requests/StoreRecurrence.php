1a2f2a38c880e729418ae643c8244fa660841416
Colin Hall <colin@macsi.co.uk>
2020-06-23 19:07:44 +0100

    Reintroduce exclude_unless

--- src/Requests/StoreRecurrence.php
+++ src/Requests/StoreRecurrence.php
@@ -14,8 +14,8 @@ class StoreRecurrence extends PersistResource
 	    "monthly_day" => "nullable|numeric|between:1,31",
 	    "monthly_week" => "nullable|numeric|in:-1,1,2,3,4",
 	    "monthly_week_day" => "nullable|integer|in:1,2,3,4,5,6,7",
-	    "end_times" => "nullable|numeric|max:50|",//exclude_unless:end_date_time,null",
-	    "end_date_time" => "nullable|date|",//exclude_unless:end_times,null"
+	    "end_times" => "nullable|numeric|max:50|exclude_unless:end_date_time,null",
+	    "end_date_time" => "nullable|date|exclude_unless:end_times,null",
     ];
-    
+
 }
