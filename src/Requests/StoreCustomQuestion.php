e3f342627fa4cc5f5e380cd3cbbbb6a38584ca6a
Colin Hall <colin@macsi.co.uk>
2020-09-01 13:40:35 +0100

    Update for Custom Questions on Registrant

---
+++ src/Requests/StoreCustomQuestion.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MacsiDigital\Zoom\Requests;
+
+use MacsiDigital\API\Support\PersistResource;
+
+class StoreCustomQuestion extends PersistResource
+{
+
+    protected $persistAttributes = [
+        'title' => 'required|string',
+        'value' => 'required|string',
+    ];
+
+}
