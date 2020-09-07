c48d2d4cc417f18969f583571d1570dc9a3a6ca7
Colin Hall <colin@macsi.co.uk>
2020-09-07 15:43:22 +0100

    Bug fix
    Change weekly_days to string from integer

--- src/Requests/StoreRecurrence.php
+++ src/Requests/StoreRecurrence.php
@@ -10,7 +10,7 @@ class StoreRecurrence extends PersistResource
     protected $persistAttributes = [
     	"type" => "nullable|integer|in:1,2,3",
 	    "repeat_interval" => "nullable|integer",
-	    "weekly_days" => "nullable|integer|in:1,2,3,4,5,6,7",
+	    "weekly_days" => "nullable|string",
 	    "monthly_day" => "nullable|numeric|between:1,31",
 	    "monthly_week" => "nullable|numeric|in:-1,1,2,3,4",
 	    "monthly_week_day" => "nullable|integer|in:1,2,3,4,5,6,7",
