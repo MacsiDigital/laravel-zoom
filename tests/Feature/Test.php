3e205e6ddcd710397561029c1e2506cd1eda3777
Colin Hall <colin@macsi.co.uk>
2021-01-05 14:41:50 +0000

    Update for PHP8.0

---
+++ tests/Feature/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MacsiDigital\Zoom\Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    /** @test */
+    public function base_test()
+    {
+        $this->assertTrue(true);
+    }
+}
