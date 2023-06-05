6da05c2d878ad82cf400de170ba30d076c8fc3d4
DevMahmoudMustafa <135623840+DevMahmoudMustafa@users.noreply.github.com>
2023-06-05 19:37:07 +0300

    Update config.php
    support Oauth

--- config/config.php
+++ config/config.php
@@ -1,10 +1,13 @@
 <?php
 
 return [
+    'account_id' => env('ZOOM_ACCOUNT_ID'),
+    'client_id' => env('ZOOM_CLIENT_ID'),
+    'client_secret' => env('ZOOM_CLIENT_SECRET'),
     'api_key' => env('ZOOM_CLIENT_KEY'),
     'api_secret' => env('ZOOM_CLIENT_SECRET'),
     'base_url' => 'https://api.zoom.us/v2/',
     'token_life' => 60 * 60 * 24 * 7, // In seconds, default 1 week
-    'authentication_method' => 'jwt', // Only jwt compatible at present but will add OAuth2
+    'authentication_method' => 'oauth',
     'max_api_calls_per_request' => '5' // how many times can we hit the api to return results for an all() request
 ];
