56ad39ee6af234a2b58858917e4933961ef6884b
DevMahmoudMustafa <135623840+DevMahmoudMustafa@users.noreply.github.com>
2023-06-05 20:52:27 +0300

    delete token life

--- config/config.php
+++ config/config.php
@@ -5,7 +5,6 @@ return [
     'client_id' => env('ZOOM_CLIENT_ID'),
     'client_secret' => env('ZOOM_CLIENT_SECRET'),
     'base_url' => 'https://api.zoom.us/v2/',
-    'token_life' => 60 * 60 * 24 * 7, // In seconds, default 1 week
     'authentication_method' => 'Oauth', // Only Oauth compatible at present
     'max_api_calls_per_request' => '5' // how many times can we hit the api to return results for an all() request
 ];
