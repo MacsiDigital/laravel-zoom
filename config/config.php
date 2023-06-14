72a504ed831753efd0b99c7aff62decd6db61b3a
DevMahmoudMustafa <135623840+DevMahmoudMustafa@users.noreply.github.com>
2023-06-15 01:18:48 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -6,4 +6,5 @@ return [
     'client_secret' => env('ZOOM_CLIENT_SECRET'),
     'base_url' => 'https://api.zoom.us/v2/',
     'authentication_method' => 'Oauth', // Only Oauth compatible at present
+    'max_api_calls_per_request' => '5' // how many times can we hit the api to return results for an all() request
 ];
