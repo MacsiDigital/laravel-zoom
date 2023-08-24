25a8c4a5c8880014249ef58b4dd05d2346030602
Colin Hall <colin@macsi.co.uk>
2023-08-24 23:22:28 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ return [
     'account_id' => env('ZOOM_ACCOUNT_ID'),
     'base_url' => 'https://api.zoom.us/v2/',
     'token_life' => 60 * 60 * 24 * 7, // In seconds, default 1 week
-    'authentication_method' => 'jwt', // you can use 'jwt' or 'oauth2' (default jwt) oauth2 is use Server-to-sever OAuth
+    'authentication_method' => 'oauth2', // you can use 'jwt' or 'oauth2' (default jwt) oauth2 is use Server-to-sever OAuth
     'max_api_calls_per_request' => '5', // how many times can we hit the api to return results for an all() request
     'site_url' => 'https://zoom.us', // This is the url for oauth2 authenication
 ];
